8519e7257c838af0b96625df1b605389432a9978
QuantumPsyche <89321433+QuantumPsyche@users.noreply.github.com>
2022-12-10 05:14:22 -0800

    Create Reroll.gg.lua

---
+++ Loadstrings/Reroll.gg.lua
@@ -0,0 +1,13 @@
+loadstring(game:HttpGet("https://www.system-exodus.com/scripts/misc-releases/Reroll.gg.lua", true))()
+
+--[[
+
+https://www.roblox.com/games/4752917845/reroll-gg-Case-Simulator
+
+- Auto Buy Cases
+- Auto Open Cases
+- Auto Sell Skins
+- Case Opening & Profit Statistics
+- Rarity Reference
+
+]]--
